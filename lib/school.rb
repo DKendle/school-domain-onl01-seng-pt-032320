92a89ec6a0822edf58323eb574657041f238b3bf
Dina Kendle <dina.kendle@gmail.com>
2020-04-09 19:47:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,34 +1,29 @@
 class School
-  attr_accessor :name, :roster
+    attr_accessor :name, :roster
   
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
  
-  def add_student(student_name, grade)
-      if @roster[grade]
-          @roster[grade] << student_name
-      else   
-          @roster[grade] = []
-          @roster[grade] << student_name
-      end
-  end
+    def add_student(student_name, grade)
+        if @roster[grade]
+            @roster[grade] << student_name
+        else   
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
   
-  def grade(grade)
-    @roster[grade]
-  end
+    def grade(grade)
+        @roster[grade]
+    end
   
-  def sort(student_name)
-    
-   @roster = @roster[grade].sort 
-   
- end
- @roster
-  end
-  
+    def sort(student_name)
+        @roster = @roster[grade].sort 
+    end
 
-  
+  @roster
   
   
 end
